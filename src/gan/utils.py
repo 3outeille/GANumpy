1eeb6717c0d6ca85dd16440416ea8f394988192c
Ferdinand Mom <ferdinand.mom@epita.fr>
2020-08-28 16:47:04 +0200

    Add utility functions

--- src/gan/utils.py
+++ src/gan/utils.py
@@ -2,6 +2,8 @@ import os
 import gzip
 import math
 import pickle
+import imageio
+import itertools
 import numpy as np
 import urllib.request
 from PIL import Image
@@ -133,7 +135,6 @@ def resize_dataset(dataset):
     res = res.reshape(-1, 1, 32, 32)
     return res
 
-
 def dataloader(X, y, BATCH_SIZE):
     """
         Returns a data generator.
@@ -145,31 +146,6 @@ def dataloader(X, y, BATCH_SIZE):
     n = len(X)
     for t in range(0, n, BATCH_SIZE):
         yield X[t:t+BATCH_SIZE, ...], y[t:t+BATCH_SIZE, ...]
-        
-def one_hot_encoding(y):
-    """
-        Performs one-hot-encoding on y.
-        
-        Parameters:
-        - y: ground truth labels.
-    """
-    N = y.shape[0]
-    Z = np.zeros((N, 10))
-    Z[np.arange(N), y] = 1
-    return Z
-
-def train_val_split(X, y, val=50000):
-    """
-        Splits X and y into training and validation set.
-
-        Parameters:
-        - X: dataset examples.
-        - y: ground truth labels.
-    """
-    X_train, X_val = X[:val, :], X[val:, :]
-    y_train, y_val = y[:val, :], y[val:, :]
-
-    return X_train, y_train, X_val, y_val
 
 def save_params_to_file(model):
     """
@@ -301,3 +277,53 @@ def plot_example_errors(X, y, y_pred):
 
     # Plot the first 9 images.
     plot_example(X, y, y_pred)
+
+def show_result(EPOCHS, path, show=False, save=False):
+    z = np.random.randn((5*5, 100))
+    test_images = G(z)
+    
+    size_figure_grid = 5
+
+    fig, ax = plt.subplots(size_figure_grid, size_figure_grid, figsize=(5, 5))
+    for i, j in itertools.product(range(size_figure_grid), range(size_figure_grid)):
+        ax[i, j].get_xaxis().set_visible(False)
+        ax[i, j].get_yaxis().set_visible(False)
+
+    for k in range(5*5):
+        i = k // 5
+        j = k % 5
+        ax[i, j].cla()
+        ax[i, j].imshow(test_images[k, :].cpu().data.view(28, 28).numpy(), cmap='gray')
+
+    label = 'Epoch {0}'.format(num_epoch)
+    fig.text(0.5, 0.04, label, ha='center')
+    plt.savefig(path)
+
+    if show:
+        plt.show()
+    else:
+        plt.close())
+
+def show_train_hist(hist, path, show=False, save=False):
+    x = range(len(hist['D_losses']))
+
+    y1 = hist['D_losses']
+    y2 = hist['G_losses']
+
+    plt.plot(x, y1, label='D_loss')
+    plt.plot(x, y2, label='G_loss')
+
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+
+    plt.legend(loc=4)
+    plt.grid(True)
+    plt.tight_layout()
+
+    if save:
+        plt.savefig(path)
+
+    if show:
+        plt.show()
+    else:
+        plt.close()
